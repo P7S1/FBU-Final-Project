7f5068363a3675be2422b6c7afcce46af8309f76
P7S1 <52729656+P7S1@users.noreply.github.com>
2021-07-15 21:13:42 -0400

    Added CollectionView to FeedViewControlelr

--- FBU Final Project/ViewControllers/Feed/FeedViewController.m
+++ FBU Final Project/ViewControllers/Feed/FeedViewController.m
@@ -9,13 +9,29 @@
 
 @interface FeedViewController ()
 
+@property (nonatomic, strong) UICollectionView *collectionView;
+
 @end
 
 @implementation FeedViewController
 
 - (void)viewDidLoad{
     [super viewDidLoad];
+    [self setUpCollectionView];
     self.view.backgroundColor = UIColor.redColor;
+}
+
+- (void)setUpCollectionView{
+    self.collectionView = [[UICollectionView alloc]init];
+    self.collectionView.translatesAutoresizingMaskIntoConstraints = NO;
+    [self.view addSubview:self.collectionView];
+    
+    [NSLayoutConstraint activateConstraints:@[
+        [self.collectionView.topAnchor constraintEqualToAnchor:self.view.topAnchor],
+        [self.collectionView.bottomAnchor constraintEqualToAnchor:self.view.bottomAnchor],
+        [self.collectionView.leftAnchor constraintEqualToAnchor:self.view.leftAnchor],
+        [self.collectionView.rightAnchor constraintEqualToAnchor:self.view.rightAnchor]
+    ]];
 }
 
 @end
