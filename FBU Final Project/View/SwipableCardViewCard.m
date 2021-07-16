c13e3ce2430dfe0308bf40a120136585de903ef7
P7S1 <52729656+P7S1@users.noreply.github.com>
2021-07-16 09:55:42 -0400

    Set up some costs on SwipableCardViewCard

--- FBU Final Project/View/SwipableCardViewCard.m
+++ FBU Final Project/View/SwipableCardViewCard.m
@@ -10,8 +10,46 @@
 
 @interface SwipableCardViewCard ()
 
+//Gesture Regozniers
+@property (nonatomic, strong, nullable) UIPanGestureRecognizer* panGestureRecognizer;
+@property (nonatomic) CGPoint panGestureTranslation;
+@property (nonatomic, strong, nullable) UITapGestureRecognizer* tapGestureRecognizer;
+
+//Drag animation settings
+@property (nonatomic) CGFloat const maximumRotation;
+@property (nonatomic) CGFloat const rotationAngle;
+@property (nonatomic) CGFloat const animationDirectionY;
+@property (nonatomic) CGFloat const swipePercentageMargin;
+
+//Card Finalise Swipe Animation
+@property (nonatomic) NSTimeInterval const finalizeSwipeActionAnimationDuration;
+
+//Card Reset Animation
+@property (nonatomic) CGFloat const cardViewResetAnimationSpringBounciness;
+@property (nonatomic) CGFloat const cardViewResetAnimationSpringSpeed;
+@property (nonatomic) CGFloat const cardViewResetAnimationDuration;
+
 @end
 
 @implementation SwipableCardViewCard
+
+- (instancetype)init{
+    self = [super init];
+    if (self) {
+        self.panGestureTranslation = CGPointZero;
+        
+        self.maximumRotation = 1.0;
+        self.rotationAngle = M_PI / 10.0;
+        self.animationDirectionY = 1.0;
+        self.swipePercentageMargin = 0.6;
+        
+        self.finalizeSwipeActionAnimationDuration = 0.8;
+        
+        self.cardViewResetAnimationSpringBounciness = 10.0;
+        self.cardViewResetAnimationSpringSpeed = 20.0;
+        self.cardViewResetAnimationDuration = 0.2;
+    }
+    return self;
+}
 
 @end
