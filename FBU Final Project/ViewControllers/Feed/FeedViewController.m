7e8a767a70e7e19aa5ccea1ca2fd908a1dc4a98e
P7S1 <52729656+P7S1@users.noreply.github.com>
2021-07-15 21:22:56 -0400

    Set up temporary mock data for FeedViewController

--- FBU Final Project/ViewControllers/Feed/FeedViewController.m
+++ FBU Final Project/ViewControllers/Feed/FeedViewController.m
@@ -6,8 +6,9 @@
 //
 
 #import "FeedViewController.h"
+#import "SwipeCollectionViewCell.h"
 
-@interface FeedViewController ()
+@interface FeedViewController ()<UICollectionViewDelegate, UICollectionViewDataSource>
 
 @property (nonatomic, strong) UICollectionView *collectionView;
 
@@ -24,6 +25,8 @@
 - (void)setUpCollectionView{
     self.collectionView = [[UICollectionView alloc]init];
     self.collectionView.translatesAutoresizingMaskIntoConstraints = NO;
+    
+    [self.collectionView registerClass:[SwipeCollectionViewCell class] forCellWithReuseIdentifier:@"SwipeCollectionViewCell"];
     [self.view addSubview:self.collectionView];
     
     [NSLayoutConstraint activateConstraints:@[
@@ -32,6 +35,42 @@
         [self.collectionView.leftAnchor constraintEqualToAnchor:self.view.leftAnchor],
         [self.collectionView.rightAnchor constraintEqualToAnchor:self.view.rightAnchor]
     ]];
+}
+
+- (NSInteger)collectionView:(UICollectionView *)collectionView numberOfItemsInSection:(NSInteger)section{
+    return 50;
+}
+
+- (__kindof UICollectionViewCell *)collectionView:(UICollectionView *)collectionView cellForItemAtIndexPath:(NSIndexPath *)indexPath{
+    SwipeCollectionViewCell *cell = [collectionView dequeueReusableCellWithReuseIdentifier:@"SwipeCollectionViewCell" forIndexPath:indexPath];
+    
+    UIColor *cellBackgroundColor;
+    NSInteger cellRemainder = indexPath.row % 5;
+    
+    switch (cellRemainder) {
+        case 0:
+            cellBackgroundColor = UIColor.systemRedColor;
+            break;
+        case 1:
+            cellBackgroundColor = UIColor.systemOrangeColor;
+            break;
+        case 2:
+            cellBackgroundColor = UIColor.systemYellowColor;
+            break;
+        case 3:
+            cellBackgroundColor = UIColor.systemGreenColor;
+            break;
+        case 4:
+            cellBackgroundColor = UIColor.systemBlueColor;
+            break;
+        default:
+            cellBackgroundColor = UIColor.systemPurpleColor;
+            break;
+    }
+    
+    cell.contentView.backgroundColor = cellBackgroundColor;
+    
+    return cell;
 }
 
 @end
