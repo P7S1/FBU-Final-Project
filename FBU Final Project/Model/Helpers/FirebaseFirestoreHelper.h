9500c5db8dc086a8f51c0dafff29ff51c3ed8223
P7S1 <52729656+P7S1@users.noreply.github.com>
2021-07-19 14:15:01 -0400

    created "FirebaseFirestoreHelper" helper class

---
+++ FBU Final Project/Model/Helpers/FirebaseFirestoreHelper.h
@@ -0,0 +1,19 @@
+//
+//  FirebaseFirestoreHelper.h
+//  FBU Final Project
+//
+//  Created by Keng Fontem on 7/19/21.
+//
+
+#import <Foundation/Foundation.h>
+#import "ItemListing.h"
+
+NS_ASSUME_NONNULL_BEGIN
+
+@interface FirebaseFirestoreHelper : NSObject
+
+
+
+@end
+
+NS_ASSUME_NONNULL_END
---
+++ FBU Final Project/Model/Helpers/FirebaseFirestoreHelper.m
@@ -0,0 +1,12 @@
+//
+//  FirebaseFirestoreHelper.m
+//  FBU Final Project
+//
+//  Created by Keng Fontem on 7/19/21.
+//
+
+#import "FirebaseFirestoreHelper.h"
+
+@implementation FirebaseFirestoreHelper
+
+@end
