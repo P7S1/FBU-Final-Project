3b78b484f069736ec70954a0ee7a5d59ec47b22b
P7S1 <52729656+P7S1@users.noreply.github.com>
2021-07-19 19:33:08 -0400

    Created the CategoryCollectionViewCell

---
+++ FBU Final Project/View/UICollectionViewCells/CategoryCollectionViewCell.h
@@ -0,0 +1,18 @@
+//
+//  CategoryCollectionViewCell.h
+//  FBU Final Project
+//
+//  Created by Keng Fontem on 7/19/21.
+//
+
+#import <UIKit/UIKit.h>
+
+NS_ASSUME_NONNULL_BEGIN
+
+@interface CategoryCollectionViewCell : UICollectionViewCell
+
+@property (nonatomic, strong) UIImageView* itemImageView;
+
+@end
+
+NS_ASSUME_NONNULL_END
