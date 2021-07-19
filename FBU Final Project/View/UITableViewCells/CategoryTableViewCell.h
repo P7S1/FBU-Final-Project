d4dcdabb7dbfbdb58f14119135d930747c1c71d6
P7S1 <52729656+P7S1@users.noreply.github.com>
2021-07-19 19:26:55 -0400

    Added collectionView and titleLabel to CategoryTableViewCell

--- FBU Final Project/View/UITableViewCells/CategoryTableViewCell.h
+++ FBU Final Project/View/UITableViewCells/CategoryTableViewCell.h
@@ -11,6 +11,8 @@ NS_ASSUME_NONNULL_BEGIN
 
 @interface CategoryTableViewCell : UITableViewCell
 
+@property (nonatomic, strong) UILabel* titleLabel;
+
 @end
 
 NS_ASSUME_NONNULL_END
--- FBU Final Project/View/UITableViewCells/CategoryTableViewCell.m
+++ FBU Final Project/View/UITableViewCells/CategoryTableViewCell.m
@@ -7,15 +7,47 @@
 
 #import "CategoryTableViewCell.h"
 
+@interface CategoryTableViewCell()
+
+@property (nonatomic, strong) UICollectionView* collectionView;
+
+@end
+
 @implementation CategoryTableViewCell
 
-- (instancetype)init
-{
+- (instancetype)init{
     self = [super init];
     if (self) {
-        
+        [self setUpCollectionView];
+        [self setUpTitleView];
     }
     return self;
+}
+
+- (void)setUpTitleView{
+    self.titleLabel = [[UILabel alloc]init];
+    self.titleLabel.text = @"Title goes here";
+    self.titleLabel.translatesAutoresizingMaskIntoConstraints = NO;
+    [self.contentView addSubview:self.titleLabel];
+    
+    [NSLayoutConstraint activateConstraints:@[
+        [self.topAnchor constraintEqualToAnchor:self.contentView.topAnchor constant:16],
+        [self.leftAnchor constraintEqualToAnchor:self.contentView.leftAnchor constant:16],
+        [self.rightAnchor constraintEqualToAnchor:self.contentView.rightAnchor constant:-16]
+    ]];
+}
+
+- (void)setUpCollectionView{
+    self.collectionView = [[UICollectionView alloc]init];
+    self.collectionView.translatesAutoresizingMaskIntoConstraints = NO;
+    [self.contentView addSubview:self.collectionView];
+    
+    [NSLayoutConstraint activateConstraints:@[
+        [self.collectionView.topAnchor constraintEqualToAnchor:self.titleLabel.bottomAnchor constant:8],
+        [self.collectionView.leftAnchor constraintEqualToAnchor:self.contentView.leftAnchor],
+        [self.collectionView.rightAnchor constraintEqualToAnchor:self.contentView.rightAnchor],
+        [self.collectionView.bottomAnchor constraintEqualToAnchor:self.contentView.bottomAnchor]
+    ]];
 }
 
 @end
