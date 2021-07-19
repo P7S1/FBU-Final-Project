16d226e66f1009e44c842e79ab93ad67e9eb851a
P7S1 <52729656+P7S1@users.noreply.github.com>
2021-07-19 19:14:54 -0400

    created skeleton for "CategoryTableViewCell"

---
+++ FBU Final Project/View/UITableViewCells/CategoryTableViewCell.h
@@ -0,0 +1,16 @@
+//
+//  CategoryTableViewCell.h
+//  FBU Final Project
+//
+//  Created by Keng Fontem on 7/19/21.
+//
+
+#import <UIKit/UIKit.h>
+
+NS_ASSUME_NONNULL_BEGIN
+
+@interface CategoryTableViewCell : UITableViewCell
+
+@end
+
+NS_ASSUME_NONNULL_END
---
+++ FBU Final Project/View/UITableViewCells/CategoryTableViewCell.m
@@ -0,0 +1,21 @@
+//
+//  CategoryTableViewCell.m
+//  FBU Final Project
+//
+//  Created by Keng Fontem on 7/19/21.
+//
+
+#import "CategoryTableViewCell.h"
+
+@implementation CategoryTableViewCell
+
+- (instancetype)init
+{
+    self = [super init];
+    if (self) {
+        
+    }
+    return self;
+}
+
+@end
