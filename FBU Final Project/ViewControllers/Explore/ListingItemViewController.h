0a979f947dc7b55d39759e059674ffa608dd0be3
P7S1 <52729656+P7S1@users.noreply.github.com>
2021-07-21 14:21:43 -0400

    Created skeleton for ListingItemViewController

---
+++ FBU Final Project/ViewControllers/Explore/ListingItemViewController.h
@@ -0,0 +1,16 @@
+//
+//  ListingItemViewController.h
+//  FBU Final Project
+//
+//  Created by Keng Fontem on 7/21/21.
+//
+
+#import <UIKit/UIKit.h>
+
+NS_ASSUME_NONNULL_BEGIN
+
+@interface ListingItemViewController : UIViewController
+
+@end
+
+NS_ASSUME_NONNULL_END
