ee84054b74fe9b7fb25bcfc15b37afcd17dcc907
P7S1 <52729656+P7S1@users.noreply.github.com>
2021-07-23 15:28:11 -0400

    Added more details to ListingItemDetailViewController

--- FBU Final Project/ViewControllers/Explore/ListingItemDetailViewController.m
+++ FBU Final Project/ViewControllers/Explore/ListingItemDetailViewController.m
@@ -10,11 +10,19 @@
 #import "ItemListing.h"
 #import "ZoomAnimatorDelegate.h"
 #import "ZoomTransitionController.h"
+#import "BasicButton.h"
+#import "DesignHelper.h"
 
 @interface ListingItemDetailViewController()<ZoomAnimatorDelegate, UIGestureRecognizerDelegate>
 
 @property (nonatomic, strong) UIImageView* imageView;
+@property (nonatomic, strong) UILabel* titleLabel;
+@property (nonatomic, strong) UILabel* priceLabel;
+@property (nonatomic, strong) UILabel* descriptionLabel;
+@property (nonatomic, strong) UILabel* dateLabel;
+@property (nonatomic, strong) BasicButton* addToCartButton;
 @property (nonatomic, strong) UIPanGestureRecognizer* panGestureRecognizer;
+
 
 @end
 
@@ -32,9 +40,14 @@
 - (void)viewDidLoad{
     [super viewDidLoad];
     [self setUpPanGestureRecognizer];
+    [self setUpInitialViewElements];
     self.view.backgroundColor = UIColor.systemBackgroundColor;
     self.navigationItem.title = self.item.name;
     
+    
+}
+
+- (void)setUpInitialViewElements{
     self.imageView = [[UIImageView alloc]init];
     self.imageView.backgroundColor = UIColor.secondarySystemBackgroundColor;
     self.imageView.contentMode = UIViewContentModeScaleAspectFill;
@@ -62,9 +75,57 @@
     self.view.layer.shadowOffset = CGSizeMake(0.0, 0.0);
     self.view.layer.shadowRadius = 12.0;
     self.view.layer.shadowOpacity = 0.7;
-}
-
-- (void)setUpInitialViewElements{
+    
+    self.titleLabel = [[UILabel alloc]init];
+    self.titleLabel.text = @"Title goes here";
+    self.titleLabel.font = [UIFont systemFontOfSize:25 weight:UIFontWeightBold];
+    self.titleLabel.translatesAutoresizingMaskIntoConstraints = NO;
+    [self.view addSubview:self.titleLabel];
+    
+    [NSLayoutConstraint activateConstraints:@[
+        [self.titleLabel.leftAnchor constraintEqualToAnchor:self.view.leftAnchor constant:16],
+        [self.titleLabel.bottomAnchor constraintEqualToAnchor:self.imageView.topAnchor constant:-8],
+    ]];
+    
+    self.priceLabel = [[UILabel alloc]init];
+    self.priceLabel.text = @"$2.99";
+    self.priceLabel.textColor = UIColor.systemGreenColor;
+    self.priceLabel.font = [UIFont systemFontOfSize:25 weight:UIFontWeightBold];
+    self.priceLabel.translatesAutoresizingMaskIntoConstraints = NO;
+    [self.view addSubview:self.titleLabel];
+    
+    self.descriptionLabel = [[UILabel alloc]init];
+    self.descriptionLabel.text = @"Description Label la la la la la la la";
+    self.descriptionLabel.translatesAutoresizingMaskIntoConstraints = NO;
+    self.descriptionLabel.textColor = UIColor.labelColor;
+    
+    self.dateLabel = [[UILabel alloc]init];
+    self.dateLabel.textColor = UIColor.secondaryLabelColor;
+    self.dateLabel.text = @"3 days ago";
+    self.dateLabel.translatesAutoresizingMaskIntoConstraints = NO;
+    
+    self.addToCartButton = [[BasicButton alloc]init];
+    [self.addToCartButton setTitle:@"Add to Cart" forState:UIControlStateNormal];
+    self.addToCartButton.backgroundColor = [DesignHelper buttonBackgroundColor];
+    [self.addToCartButton setTitleColor:[DesignHelper buttonTitleLabelColor] forState:UIControlStateNormal];
+    [[self.addToCartButton.heightAnchor constraintEqualToConstant:50.0] setActive:YES];
+    
+    UIStackView* stackView = [[UIStackView alloc]initWithArrangedSubviews:@[
+        self.priceLabel,
+        self.descriptionLabel,
+        self.dateLabel,
+        self.addToCartButton
+    ]];
+    stackView.translatesAutoresizingMaskIntoConstraints = NO;
+    stackView.axis = UILayoutConstraintAxisVertical;
+    
+    [self.view addSubview:stackView];
+    
+    [NSLayoutConstraint activateConstraints:@[
+        [stackView.leftAnchor constraintEqualToAnchor:self.view.leftAnchor constant:16],
+        [stackView.topAnchor constraintEqualToAnchor:self.imageView.bottomAnchor constant:8],
+        [stackView.rightAnchor constraintEqualToAnchor:self.view.rightAnchor constant:-16]
+    ]];
     
     
 }
@@ -86,7 +147,6 @@
             [self handlePangestureRecognizerEnded:gestureRecognizer];
             break;
         default:
-            [self handlePangestureRecognizerDefault:gestureRecognizer];
             if (self.transitionController.isInteractive){
                 [self.transitionController didPanWith:gestureRecognizer];
             }
@@ -106,17 +166,13 @@
     }
 }
 
-- (void)handlePangestureRecognizerDefault: (UIPanGestureRecognizer*)gestureRecognizer{
-    
-}
-
 //MARK:- ZoomAnimatorDelegate
 - (UIImageView *)refereneImageViewFor:(ZoomAnimator *)zoomAnimator{
     return self.imageView;
 }
 
 - (CGRect)refereneImageViewFrameInTransitioningViewFor:(ZoomAnimator *)zoomAnimator{
-    return CGRectMake(0, 162.667, UIScreen.mainScreen.bounds.size.width, UIScreen.mainScreen.bounds.size.width * 1.33);
+    return CGRectMake(0, 32, UIScreen.mainScreen.bounds.size.width, UIScreen.mainScreen.bounds.size.width * 1.33);
 }
 
 - (void)transitionDidEndWith:(ZoomAnimator *)zoomAnimator {}
