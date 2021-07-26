cff1643756bb7ae858c252257fd5da9b90fb417d
P7S1 <52729656+P7S1@users.noreply.github.com>
2021-07-26 13:21:25 -0400

    Made it so the actual details of the items showing the details VC

--- FBU Final Project/ViewControllers/Explore/ListingItemDetailViewController.m
+++ FBU Final Project/ViewControllers/Explore/ListingItemDetailViewController.m
@@ -74,7 +74,7 @@
     self.view.layer.shadowOpacity = 0.7;
     
     self.titleLabel = [[UILabel alloc]init];
-    self.titleLabel.text = @"Title goes here";
+    self.titleLabel.text = self.item.name;
     self.titleLabel.font = [UIFont systemFontOfSize:25 weight:UIFontWeightBold];
     self.titleLabel.translatesAutoresizingMaskIntoConstraints = NO;
     [self.view addSubview:self.titleLabel];
@@ -85,20 +85,20 @@
     ]];
     
     self.priceLabel = [[UILabel alloc]init];
-    self.priceLabel.text = @"$2.99";
+    self.priceLabel.text = [@"$" stringByAppendingString:@(self.item.price).stringValue];
     self.priceLabel.textColor = UIColor.systemGreenColor;
     self.priceLabel.font = [UIFont systemFontOfSize:25 weight:UIFontWeightBold];
     self.priceLabel.translatesAutoresizingMaskIntoConstraints = NO;
     [self.view addSubview:self.titleLabel];
     
     self.descriptionLabel = [[UILabel alloc]init];
-    self.descriptionLabel.text = @"Description Label la la la la la la la";
+    self.descriptionLabel.text = self.item.description;
     self.descriptionLabel.translatesAutoresizingMaskIntoConstraints = NO;
     self.descriptionLabel.textColor = UIColor.labelColor;
     
     self.dateLabel = [[UILabel alloc]init];
     self.dateLabel.textColor = UIColor.secondaryLabelColor;
-    self.dateLabel.text = @"3 days ago";
+    self.dateLabel.text = [self getDateLabelTextWithDate:[self.item.endsInTimestamp dateValue]];
     self.dateLabel.translatesAutoresizingMaskIntoConstraints = NO;
     
     self.addToCartButton = [[BasicButton alloc]init];
@@ -123,6 +123,39 @@
         [stackView.topAnchor constraintEqualToAnchor:self.imageView.bottomAnchor constant:8],
         [stackView.rightAnchor constraintEqualToAnchor:self.view.rightAnchor constant:-16]
     ]];
+}
+
+- (NSString*)getDateLabelTextWithDate: (NSDate*)date{
+    NSDateComponentsFormatter *formatter = [[NSDateComponentsFormatter alloc] init];
+    formatter.unitsStyle = NSDateComponentsFormatterUnitsStyleFull;
+
+    NSDate *now = [NSDate date];
+
+    NSCalendar *calendar = [NSCalendar currentCalendar];
+    NSDateComponents *components = [calendar components:(NSCalendarUnitYear|NSCalendarUnitMonth|NSCalendarUnitWeekOfMonth|NSCalendarUnitDay|NSCalendarUnitHour|NSCalendarUnitMinute|NSCalendarUnitSecond)
+                                               fromDate:date
+                                                 toDate:now
+                                                options:0];
+
+    if (components.year > 0) {
+        formatter.allowedUnits = NSCalendarUnitYear;
+    } else if (components.month > 0) {
+        formatter.allowedUnits = NSCalendarUnitMonth;
+    } else if (components.weekOfMonth > 0) {
+        formatter.allowedUnits = NSCalendarUnitWeekOfMonth;
+    } else if (components.day > 0) {
+        formatter.allowedUnits = NSCalendarUnitDay;
+    } else if (components.hour > 0) {
+        formatter.allowedUnits = NSCalendarUnitHour;
+    } else if (components.minute > 0) {
+        formatter.allowedUnits = NSCalendarUnitMinute;
+    } else {
+        formatter.allowedUnits = NSCalendarUnitSecond;
+    }
+
+    NSString *formatString = NSLocalizedString(@"Ends in %@", @"Used to say how much time has passed. e.g. '2 hours ago'");
+
+    return [NSString stringWithFormat:formatString, [formatter stringFromDateComponents:components]];
 }
 
 //MARK:- UIPanGestureRecognizer
