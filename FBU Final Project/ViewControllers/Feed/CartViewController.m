46fc145c17d5d873ec4b2300a94178d9c50147a3
P7S1 <52729656+P7S1@users.noreply.github.com>
2021-07-28 14:57:17 -0400

    Added constraints to tableView

--- FBU Final Project/ViewControllers/Feed/CartViewController.m
+++ FBU Final Project/ViewControllers/Feed/CartViewController.m
@@ -7,7 +7,7 @@
 
 #import "CartViewController.h"
 
-@interface CartViewController ()
+@interface CartViewController()<UITableViewDelegate, UITableViewDataSource>
 
 @property (nonatomic, strong) UITableView* tableView;
 
@@ -21,7 +21,17 @@
 }
 
 - (void)setUpTableView{
+    self.tableView = [[UITableView alloc]init];
+    self.tableView.delegate = self;
+    self.tableView.dataSource = self;
+    self.tableView.translatesAutoresizingMaskIntoConstraints = NO;
     
+    [NSLayoutConstraint activateConstraints: @[
+        [self.tableView.topAnchor constraintEqualToAnchor:self.view.topAnchor],
+        [self.tableView.leftAnchor constraintEqualToAnchor:self.view.leftAnchor],
+        [self.tableView.rightAnchor constraintEqualToAnchor:self.view.rightAnchor],
+        [self.tableView.bottomAnchor constraintEqualToAnchor:self.view.bottomAnchor]
+    ]];
 }
 
 @end
