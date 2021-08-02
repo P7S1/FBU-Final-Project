a466b550d4d39519a51a3a022ee608b79f4642f9
P7S1 <52729656+P7S1@users.noreply.github.com>
2021-08-02 16:57:19 -0400

    added pipPositions with custom getter

--- FBU Final Project/View/UIKit Subclasses/PipContainerView.m
+++ FBU Final Project/View/UIKit Subclasses/PipContainerView.m
@@ -9,6 +9,8 @@
 
 @interface PipContainerView()
 
+@property (readonly) NSArray* pipPositions;
+
 @end
 
 @implementation PipContainerView
@@ -25,6 +27,7 @@ const CGFloat _verticalSpacing = 25;
 CGPoint _initialOffset;
 UIPanGestureRecognizer* _panRecognizer;
 
+//MARK:- Init Methods
 - (instancetype)initWithFrame: (CGRect)frame{
     self = [super initWithFrame:frame];
     if (self){
@@ -45,6 +48,19 @@ UIPanGestureRecognizer* _panRecognizer;
     [self setUpInitialValues];
     [self setUpPlaceholderViews];
 }
+
+//MARK:- Varible Getters and Setters
+
+- (NSArray*)pipPositions{
+    NSArray* values = [[NSArray alloc]init];
+    for (const UIView* view in _pipPositionViews){
+        NSValue* value = [NSValue valueWithCGPoint:view.center];
+        values = [values arrayByAddingObject:value];
+    }
+    return values;
+}
+
+//MARK:- Init helper methods
 
 - (void)setUpInitialValues{
     _pipPositionViews = [[NSArray alloc]init];
@@ -99,12 +115,20 @@ UIPanGestureRecognizer* _panRecognizer;
     return pipView;
 }
 
+//MARK:- Pip Animation Logic
+
 - (void)pipPannedWithRecognizer: (UIPanGestureRecognizer*)recognizer{
     
 }
 
 - (CGFloat)projectWithInitialVelocity: (CGFloat)initialVelocity withDecelerationRate: (CGFloat)declerationRate{
     return (initialVelocity / 1000.0) * declerationRate / (1 - declerationRate);
+}
+
+- (CGPoint)nearestCornerToPoint: (CGPoint)point{
+    CGFloat minDistance = CGFLOAT_MAX;
+    CGPoint closestPosition = CGPointZero;
+    
 }
 
 @end
