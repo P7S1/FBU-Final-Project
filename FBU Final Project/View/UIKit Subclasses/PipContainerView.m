485473ae8dff4bc16cd4fd2091d7295db9a0445a
P7S1 <52729656+P7S1@users.noreply.github.com>
2021-08-02 16:30:47 -0400

    Added constraints to pip container views

--- FBU Final Project/View/UIKit Subclasses/PipContainerView.m
+++ FBU Final Project/View/UIKit Subclasses/PipContainerView.m
@@ -9,8 +9,6 @@
 
 @interface PipContainerView()
 
-@property (nonatomic, strong) NSArray<UIView*>* pipPositionViews;
-
 @end
 
 @implementation PipContainerView
@@ -24,19 +22,53 @@ const CGFloat _pipHeight = 130;
 const CGFloat _horizontalSpacing = 23;
 const CGFloat _verticalSpacing = 25;
 
+CGPoint _initialOffset;
+UIPanGestureRecognizer* _panRecognizer;
+
 - (instancetype)initWithFrame: (CGRect)frame{
     self = [super initWithFrame:frame];
     if (self) {
         [self setUpInitialValues];
-        [self createPipView];
+        [self setUpPlaceholderViews];
     }
     return self;
 }
 
 - (void)setUpInitialValues{
-    self.pipPositionViews = [[NSArray alloc]init];
+    _pipPositionViews = [[NSArray alloc]init];
+    
+    _initialOffset = CGPointZero;
+    
+    _panRecognizer = [[UIPanGestureRecognizer alloc]initWithTarget:self action:@selector(pipPannedWithRecognizer:)];
+    
     _pipView = [[UIView alloc]init];
     _pipView.backgroundColor = UIColor.greenColor;
+    [self addSubview:_pipView];
+    _pipView.translatesAutoresizingMaskIntoConstraints = NO;
+    [NSLayoutConstraint activateConstraints:@[
+        [_pipView.heightAnchor constraintEqualToConstant:_pipHeight],
+        [_pipView.widthAnchor constraintEqualToConstant:_pipWidth]
+    ]];
+    
+    [_pipView addGestureRecognizer:_panRecognizer];
+}
+
+- (void)setUpPlaceholderViews{
+    const UIView* topLeftView = [self getPipView];
+    const UIView* topRightView = [self getPipView];
+    const UIView* bottomLeftView = [self getPipView];
+    const UIView* bottomRightView = [self getPipView];
+    
+    [NSLayoutConstraint activateConstraints:@[
+        [topLeftView.topAnchor constraintEqualToAnchor:self.topAnchor constant:_verticalSpacing],
+        [topLeftView.leftAnchor constraintEqualToAnchor:self.leftAnchor constant:_horizontalSpacing],
+        [topRightView.topAnchor constraintEqualToAnchor:self.topAnchor constant:_verticalSpacing],
+        [topRightView.rightAnchor constraintEqualToAnchor:self.rightAnchor constant:-_horizontalSpacing],
+        [bottomLeftView.bottomAnchor constraintEqualToAnchor:self.bottomAnchor constant:-_verticalSpacing],
+        [bottomLeftView.leftAnchor constraintEqualToAnchor:self.leftAnchor constant:_horizontalSpacing],
+        [bottomRightView.bottomAnchor constraintEqualToAnchor:self.bottomAnchor constant:-_verticalSpacing],
+        [bottomRightView.rightAnchor constraintEqualToAnchor:self.rightAnchor constant:-_horizontalSpacing]
+    ]];
 }
 
 - (UIView*)getPipView{
@@ -50,9 +82,13 @@ const CGFloat _verticalSpacing = 25;
         [pipView.widthAnchor constraintEqualToConstant:_pipWidth]
     ]];
     
-    _pipPositionViews = [self.pipPositionViews arrayByAddingObject:pipView];
+    _pipPositionViews = [_pipPositionViews arrayByAddingObject:pipView];
     
     return pipView;
+}
+
+- (void)pipPannedWithRecognizer: (UIPanGestureRecognizer*)recognizer{
+    
 }
 
 @end
