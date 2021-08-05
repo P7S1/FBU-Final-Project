aecf8e312ea1c088efea5eed20d264b567685a78
P7S1 <52729656+P7S1@users.noreply.github.com>
2021-08-05 15:07:08 -0400

    Added emoji emitter

--- FBU Final Project/ViewControllers/Login/WelcomeViewController.m
+++ FBU Final Project/ViewControllers/Login/WelcomeViewController.m
@@ -19,6 +19,7 @@
 - (void)viewDidLoad{
     [super viewDidLoad];
     [self setUpUI];
+    [self startEmitter];
 }
 
 - (void)setUpUI{
@@ -60,6 +61,60 @@
     EnterPhoneNumberViewController *vc = [[EnterPhoneNumberViewController alloc]init];
     [self.navigationController pushViewController:vc animated:YES];
 }
+
+//MARK:- Some fun stuff...CAEmitterLayer
+-(void)startEmitter{
+    UIView *emitterView = [[UIView alloc] init];
+    CAEmitterLayer *emitterLayer = [[CAEmitterLayer alloc] init];
+    
+    emitterLayer.emitterPosition = CGPointMake(self.view.frame.size.width/2, -40);
+    emitterLayer.emitterSize = CGSizeMake(self.view.frame.size.width, 1);
+    emitterLayer.emitterShape = kCAEmitterLayerLine;
+    emitterLayer.emitterCells = [self getEmitterCells];
+    //emitterLayer.birthRate = 0.125;
+    
+    [emitterView.layer addSublayer:emitterLayer];
+    emitterView.backgroundColor = UIColor.clearColor;
+    emitterView.alpha = 0.7;
+    [self.view addSubview:emitterView];
+    [self.view sendSubviewToBack:emitterView];
+    
+    [NSLayoutConstraint activateConstraints:@[
+        [emitterView.topAnchor constraintEqualToAnchor:self.view.topAnchor],
+        [emitterView.bottomAnchor constraintEqualToAnchor:self.view.bottomAnchor],
+        [emitterView.leftAnchor constraintEqualToAnchor:self.view.leftAnchor],
+        [emitterView.rightAnchor constraintEqualToAnchor:self.view.rightAnchor],
+    ]];
+    emitterView.translatesAutoresizingMaskIntoConstraints = false;
+}
+
+/*Gets emitter cells
+ - Emoji Id is the corresponding emoji in Assets
+ 
+ */
+- (NSMutableArray<CAEmitterCell *> *) getEmitterCells{
+    NSMutableArray *cells = [[NSMutableArray alloc] init];
+    
+    for (int i = 0; i < 5; i++){
+        CAEmitterCell *cell = [[CAEmitterCell alloc] init];;
+        cell.birthRate = 0.5;
+        cell.lifetime = 20;
+        cell.velocity = arc4random_uniform(50) + 50;
+        cell.scale = 0.1;
+        cell.scaleRange = 0.005;
+        cell.emissionRange = M_PI/4;
+        cell.emissionLatitude = (180 * (M_PI / 180));
+        cell.alphaRange = 0.3;
+        cell.yAcceleration = arc4random_uniform(10) + 10;
+
+        NSString *emojiId = @"emoji";
+        cell.contents = (id) [[UIImage imageNamed:emojiId] CGImage];
+        [cells addObject:cell];
+    }
+    
+    return cells;
+}
+
 
 
 @end
